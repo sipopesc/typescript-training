9a5605969748ec728ee66aef7dee484e3d877731
Silviu Constantin Popescu <constantinsilviupopescu@gmail.com>
2022-07-04 13:54:04 +0300

    simple-variables

---
+++ 01-helloworld/myhelloworld.js
@@ -0,0 +1,14 @@
+// Declaring variables
+// TypeSript is strongly typed language
+var found = true;
+var grade = 88.6;
+var firstName = 'Silviu';
+var lastName = 'Popescu';
+var myAnyData = 5.0;
+// If I later try the following assignment:
+// grade = 'A'; -> compilation error
+myAnyData = 'Eric';
+//Use "any" data type for generics: list, array, that can have different types of data
+console.log('Hello world');
+console.log('Hi ' + firstName + ' ' + lastName);
+console.log("Hi ".concat(firstName, " ").concat(lastName));
---
+++ 02-variables/sample-types.ts
@@ -0,0 +1,17 @@
+// Declaring variables
+// TypeSript is strongly typed language
+let found: boolean = true;
+let grade: number = 88.6;
+let firstName: string = 'Silviu';
+let lastName: string = 'Popescu';
+let myAnyData: any = 5.0;
+// If I later try the following assignments:
+// grade = 'A'; -> compilation error
+// found = 10; -> compilation error
+// Nevertheless, the js file will be generated, you need to use: tsc -noEmitOnError sample-types.ts to prevent js file generation
+myAnyData = 'Eric';
+//Use "any" data type for generics: list, array, that can have different types of data
+
+// sample print to console
+console.log('Hi ' + firstName + ' ' + lastName);
+console.log(`Hi ${firstName} ${lastName}`); // using template string
